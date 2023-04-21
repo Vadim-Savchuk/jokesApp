63ed7cdd916bbbcb2ae31b185301075073cda981
Vadim-Savchuk <VADIMSAVCUK38@GMAIL.COM>
2023-04-21 18:04:02 +0300

    solved the problem with the menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,27 +9,25 @@ import about from  './img/information.png';
 import user  from  './img/user.png';
 
 function Header() {
-    const [ width, setWidth ] = useState(false);
+    const [ iconst, setIconst ] = useState(false);
 
     useState(() => {
-        window.addEventListener('resize', function() {
-            if (window.innerWidth < 590) {
-              setWidth(true)
+        const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+            if (width < 590) {
+                setIconst(true)
             } else {
-                setWidth(false)
+                setIconst(false)
             }
-        });
-
-    }, [width])
+    }, [iconst])
 
     return (
         <header className='header'>
             <nav>
                 <ul className='nav-list'>
-                    <li><NavLink to='/'        className='button'>{width ? <img src={home} alt="Home" /> : 'Головна'}</NavLink></li>
-                    <li><NavLink to='/add'     className='button'>{width ? <img src={add} alt="Add" /> : 'Додати'}</NavLink></li>
-                    <li><NavLink to='/about'   className='button'>{width ? <img src={about} alt="About" /> : 'Про нас'}</NavLink></li>
-                    <li><NavLink to='/account' className='button button--pos'>{width ? <img src={user} alt="User" /> : 'Профіль'}</NavLink></li>
+                    <li><NavLink to='/'        className='button'>{iconst ? <img src={home} alt="Home" /> : 'Головна'}</NavLink></li>
+                    <li><NavLink to='/add'     className='button'>{iconst ? <img src={add} alt="Add" /> : 'Додати'}</NavLink></li>
+                    <li><NavLink to='/about'   className='button'>{iconst ? <img src={about} alt="About" /> : 'Про нас'}</NavLink></li>
+                    <li><NavLink to='/account' className='button button--pos'>{iconst ? <img src={user} alt="User" /> : 'Профіль'}</NavLink></li>
                 </ul>
             </nav>
         </header>
